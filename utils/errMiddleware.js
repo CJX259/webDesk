e77a72c926a32ff431df1465bae6bd645652e408
陈建希 <1049602251@qq.com>
2021-03-07 21:52:41 +0800

    新增icon提示错误

--- utils/errMiddleware.js
+++ utils/errMiddleware.js
@@ -2,7 +2,7 @@ const { getErr } = require("./sendResult");
 
 module.exports = (err, req, res, next)=>{
   if(err){
-    res.status(500).send(getErr(err instanceof Error ? err.message: err, 500));
+    res.send(getErr(err instanceof Error ? err.message: err, 500));
     return null;
   }else{
     next();
